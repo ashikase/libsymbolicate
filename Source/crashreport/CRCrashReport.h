0cc08fb939915aec0a35c06f53c5a1b0362f527a
Lance Fetters <gaizin@gmail.com>
2014-07-08 13:57:08 +0900

    Removed unneeded defines/guards

--- Source/crashreport/CRCrashReport.h
+++ Source/crashreport/CRCrashReport.h
@@ -1,9 +1,5 @@
 @class NSString, NSDictionary;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 extern NSString * const kCrashReportBlame;
 extern NSString * const kCrashReportDescription;
 
@@ -29,12 +25,5 @@ extern NSString * const kCrashReportDescription;
 - (BOOL)symbolicateUsingSymbolMaps:(NSDictionary *)symbolMaps;
 - (BOOL)writeToFile:(NSString *)filepath forcePropertyList:(BOOL)forcePropertyList;
 @end
-
-NSString *symbolicate(NSString *content, NSDictionary *symbolMaps, unsigned progressStepping, NSArray **blameInfo);
-NSArray *blame(NSString *exceptionType, NSDictionary *binaryImages, NSArray *backtraceLines);
-
-#ifdef __cplusplus
-}
-#endif
 
 /* vim: set ft=objc ff=unix sw=4 ts=4 tw=80 expandtab: */
