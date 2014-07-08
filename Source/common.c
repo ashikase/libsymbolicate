0bb9d22a1ecaebaab399cac800fb4064ea4b54ac
Lance Fetters <gaizin@gmail.com>
2014-07-08 14:40:14 +0900

    Removed comment block, added modeline

--- Source/common.c
+++ Source/common.c
@@ -1,23 +1,3 @@
-/*
-
-common.c ... Some simple functions.
-Copyright (C) 2009  KennyTM~ <kennytm@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 static unsigned char nibble(char c) {
     if (c >= '0' && c <= '9') {
         return c - '0';
@@ -41,3 +21,5 @@ unsigned long long unsignedLongLongFromHexString(const char* str, int len) {
     }
     return res;
 }
+
+/* vim: set ft=c ff=unix sw=4 ts=4 tw=80 expandtab: */
